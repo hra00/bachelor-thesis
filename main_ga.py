9aa4ec535a4c8a5d3b7c3176b1e75a9d12f8c356
hyeri <hyerian96@gmail.com>
2021-11-28 08:30:22 +0100

    option:diversity control added

--- main_ga.py
+++ main_ga.py
@@ -1,12 +1,14 @@
 import copy
 import random
-import numpy
+import numpy as np
 import rooms
 import agent as a
 import matplotlib.pyplot as plot
-import genitic_algorithm as ga
+import genitic_algorithm as gena
 
-
+"""
+episode without subgoals
+"""
 def episode(env, agent, nr_episode=0):
     state = env.reset()
     discounted_return = 0
@@ -27,7 +29,10 @@ def episode(env, agent, nr_episode=0):
     return discounted_return
 
 
-def episode_with_ga(env, agent, subgoal, g, i, it):
+"""
+episode with subgoals
+"""
+def episode_with_ga(env, agent, subgoal, g, i, it, diversity_control, dist):
     state = env.reset()
     env.set_subgoals(subgoal)
     discounted_return = 0
@@ -47,30 +52,33 @@ def episode_with_ga(env, agent, subgoal, g, i, it):
         discounted_return += reward * (discount_factor ** time_step)
         time_step += 1
     if done and env.agent_position == env.goal_position:
-        fitness = 1 - (time_step / 100)
+        if diversity_control:
+            fitness = 1 - (time_step / 100) + dist[i]/max(dist)
+        else:
+            fitness = 1 - (time_step / 100)
         discounted_return_without_subgoals = reward * (discount_factor ** time_step)
         save_video_g_i_it(env, g, i, it)
     return [discounted_return, fitness, discounted_return_without_subgoals]
 
 
-def subgoal_evolution(env, agent, ga, nr_generation, num_iteration):
+def subgoal_evolution(env, agent, ga, n_generation, n_iteration, diversity_control):
     discounted_returns_xsg_g = []
     discounted_returns_g = []
     fitnesses_g = []
-    for i in range(nr_generation):
+    for i in range(n_generation):  # for each generation
         discounted_returns_xsg_i = []  # discounted returns without subgoal in each generation
         discounted_returns_i = []
-        for j in range(len(ga.population)):
-            sum_dc_j, sum_fitness_j, sum_dc_xsg_j = 0, 0, 0
-            returns = [episode_with_ga(env, agent, ga.population[j], i, j, it) for it in range(num_iteration)]
-            print(ga.population[j], returns)
-            for discounted_return_it, fitness_it, dc_xsg_it in returns:
-                sum_dc_j += discounted_return_it
-                sum_fitness_j += fitness_it
-                sum_dc_xsg_j += dc_xsg_it
-            ga.fitnesses.append(sum_fitness_j / num_iteration)
-            discounted_returns_i.append(sum_dc_j / num_iteration)
-            discounted_returns_xsg_i.append(sum_dc_xsg_j / num_iteration)
+        # for each j in generation, calculate distance between j and population (if diversity_control = true)
+        dist = [gena.dist_inp(individual, ga.population, 10) for individual in ga.population] if diversity_control else []
+        for j in range(len(ga.population)):  # for each individual in population
+            # iteration
+            returns = [episode_with_ga(env, agent, ga.population[j], i, j, it, diversity_control, dist) for it in range(n_iteration)]
+            print("g :", i, " j : ",ga.population[j], returns)
+            # mean returns for individual j
+            mean_dc_j, mean_fitness_j, mean_dc_xsg_j = np.mean(returns, axis=0)
+            discounted_returns_i.append(mean_dc_j)
+            discounted_returns_xsg_i.append(mean_dc_xsg_j)
+            ga.fitnesses.append(mean_fitness_j)
         fitnesses_g = copy.deepcopy(ga.fitnesses)
         ga.next_generation(env, prop_elite, prob_mutation, prop_offsprings)
         discounted_returns_xsg_g.append(max(discounted_returns_xsg_i))
@@ -82,8 +90,11 @@ def save_video_g_i_it(env, g, i, it):
     env.save_video()
 
 
+"""
+parameters for agent & environment
+"""
 params = {}
-env = rooms.load_env("layouts/rooms_11_11_4.txt", "rooms.mp4")
+env = rooms.load_env("layouts/rooms_9_9_4.txt", "rooms.mp4")
 params["nr_actions"] = env.action_space.n
 params["gamma"] = 0.99
 params["horizon"] = 10
@@ -92,18 +103,27 @@ params["env"] = env
 
 agent = a.MonteCarloTreeSearchPlanner(params)
 
-population_size = 20
-num_subgoals = 2
-ga = ga.initial_population(env, population_size, num_subgoals)
+
+"""
+parameters for genetic algorithms
+"""
+population_size = 10
+n_subgoals = 2
+init_p = gena.initial_population(env, population_size, n_subgoals)
 prop_elite = 0.1
 prob_mutation = 0.4
 prop_offsprings = 0.5
-nr_generation = 20
-num_iteration = 5
+n_generation = 10
+n_iteration = 1
+diversity_control = True
 
-results = subgoal_evolution(env, agent, ga, nr_generation, num_iteration)
+
+"""
+extract results
+"""
+results = subgoal_evolution(env, agent, init_p, n_generation, n_iteration, diversity_control)
 print(results)
-x = range(nr_generation)
+x = range(n_generation)
 y = results[0]
 
 plot.plot(x, y)
